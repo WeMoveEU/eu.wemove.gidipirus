057adf00ae7058d6be01d984422572d6f8a5dcef
scardinius <scardinius@chords.pl>
2018-08-29 09:50:15 +0200

    Install Request channel options

---
+++ CRM/Gidipirus/Model/RequestChannel.php
@@ -0,0 +1,116 @@
+<?php
+
+class CRM_Gidipirus_Model_RequestChannel extends CRM_Gidipirus_Model {
+
+  const OPTION_GROUP_TITLE = 'Request Channel';
+
+  const EMAIL = 'email';
+  const PHONE = 'phone';
+  const PERSONAL = 'personal';
+  const PAPER_LETTER = 'paper letter';
+  const EXPIRED = 'Expired';
+
+  /**
+   * Get Request channel - Ready
+   *
+   * @return int|mixed
+   * @throws \CiviCRM_API3_Exception
+   */
+  public static function email() {
+    $key = __CLASS__ . __FUNCTION__;
+    $cache = Civi::cache()->get($key);
+    if (!isset($cache)) {
+      $id = self::set(self::EMAIL);
+      Civi::cache()->set($key, $id);
+      return $id;
+    }
+    return $cache;
+  }
+
+  /**
+   * Get Request channel - In Progress
+   *
+   * @return int|mixed
+   * @throws \CiviCRM_API3_Exception
+   */
+  public static function phone() {
+    $key = __CLASS__ . __FUNCTION__;
+    $cache = Civi::cache()->get($key);
+    if (!isset($cache)) {
+      $id = self::set(self::PHONE);
+      Civi::cache()->set($key, $id);
+      return $id;
+    }
+    return $cache;
+  }
+
+  /**
+   * Get Request channel - Obsolete
+   *
+   * @return int|mixed
+   * @throws \CiviCRM_API3_Exception
+   */
+  public static function personal() {
+    $key = __CLASS__ . __FUNCTION__;
+    $cache = Civi::cache()->get($key);
+    if (!isset($cache)) {
+      $id = self::set(self::PERSONAL);
+      Civi::cache()->set($key, $id);
+      return $id;
+    }
+    return $cache;
+  }
+
+  /**
+   * Get Request channel - Completed
+   *
+   * @return int|mixed
+   * @throws \CiviCRM_API3_Exception
+   */
+  public static function paperLetter() {
+    $key = __CLASS__ . __FUNCTION__;
+    $cache = Civi::cache()->get($key);
+    if (!isset($cache)) {
+      $id = self::set(self::PAPER_LETTER);
+      Civi::cache()->set($key, $id);
+      return $id;
+    }
+    return $cache;
+  }
+
+  /**
+   * Get Request channel - Blocked
+   *
+   * @return int|mixed
+   * @throws \CiviCRM_API3_Exception
+   */
+  public static function expired() {
+    $key = __CLASS__ . __FUNCTION__;
+    $cache = Civi::cache()->get($key);
+    if (!isset($cache)) {
+      $id = self::set(self::EXPIRED);
+      Civi::cache()->set($key, $id);
+      return $id;
+    }
+    return $cache;
+  }
+
+  /**
+   * @return int
+   * @throws \CiviCRM_API3_Exception
+   */
+  public static function installOptionGroup() {
+    return self::optionGroup(self::OPTION_GROUP_TITLE);
+  }
+
+  /**
+   * @param $name
+   *
+   * @return int
+   * @throws \CiviCRM_API3_Exception
+   */
+  private static function set($name) {
+    return self::optionValue(self::sanitize(self::OPTION_GROUP_TITLE), $name);
+  }
+
+}
--- CRM/Gidipirus/Upgrader.php
+++ CRM/Gidipirus/Upgrader.php
@@ -22,6 +22,12 @@ class CRM_Gidipirus_Upgrader extends CRM_Gidipirus_Upgrader_Base {
     CRM_Gidipirus_Model_ForgetmeStatus::invalidRequest();
     CRM_Gidipirus_Model_ForgetmeStatus::tooManyRequests();
 
+    CRM_Gidipirus_Model_RequestChannel::installOptionGroup();
+    CRM_Gidipirus_Model_RequestChannel::email();
+    CRM_Gidipirus_Model_RequestChannel::phone();
+    CRM_Gidipirus_Model_RequestChannel::personal();
+    CRM_Gidipirus_Model_RequestChannel::paperLetter();
+    CRM_Gidipirus_Model_RequestChannel::expired();
 
     return TRUE;
   }
