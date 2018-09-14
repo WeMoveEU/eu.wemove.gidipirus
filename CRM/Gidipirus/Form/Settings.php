b537c62facf3c4488a67c8f0f0781c9a18361850
scardinius <scardinius@chords.pl>
2018-09-14 14:35:30 +0200

    Add form for settings

---
+++ CRM/Gidipirus/Form/Settings.php
@@ -0,0 +1,71 @@
+<?php
+
+use CRM_Gidipirus_ExtensionUtil as E;
+require_once 'CRM/Core/Form.php';
+
+/**
+ * Form controller class
+ *
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
+ */
+class CRM_Gidipirus_Form_Settings extends CRM_Core_Form {
+  public $fields = [];
+
+  public function __construct($state = NULL, $action = CRM_Core_Action::NONE, $method = 'post', $name = NULL) {
+    $this->fields = [
+      'scheduled_days' => [
+        'type' => 'text',
+        'label' => E::ts("Scheduled days"),
+        'options' => [],
+        'required' => TRUE,
+        'default' => CRM_Gidipirus_Settings::scheduledDays(),
+        'order' => 10,
+      ],
+    ];
+    parent::__construct($state, $action, $method, $name);
+  }
+
+  public function setDefaultValues() {
+    $defaults = array();
+    foreach ($this->fields as $key => $setting) {
+      if (array_key_exists('default', $setting)) {
+        $defaults[$key] = $setting['default'];
+      }
+    }
+    return $defaults;
+  }
+
+  public function buildQuickForm() {
+    CRM_Utils_System::setTitle(E::ts('Gidipirus Settings Page'));
+    foreach ($this->fields as $key => $field) {
+      $this->add($field['type'], $key, $field['label'], ['' => '- select -'] + $field['options'], $field['required'], @$field['extra']);
+    }
+    $this->addButtons([['type' => 'submit', 'name' => ts('Submit'), 'isDefault' => TRUE]]);
+    $this->assign('elementNames', $this->getRenderableElementNames());
+    parent::buildQuickForm();
+  }
+
+  public function postProcess() {
+    $values = $this->exportValues();
+    CRM_Gidipirus_Settings::scheduledDays($values['scheduled_days']);
+    CRM_Core_Session::setStatus(E::ts('Settings are updated'), 'Gidipirus', 'success');
+  }
+
+  /**
+   * Get the fields/elements defined in this form.
+   *
+   * @return array (string)
+   */
+  private function getRenderableElementNames() {
+    $elementNames = array();
+    foreach ($this->_elements as $element) {
+      /** @var HTML_QuickForm_Element $element */
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+    return $elementNames;
+  }
+
+}
