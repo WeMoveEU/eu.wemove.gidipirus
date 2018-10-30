b013be4b22dd344cd5444081e3c3aed8cec342c6
scardinius <scardinius@chords.pl>
2018-10-30 16:11:26 +0100

    Set register link close to Inbound Email

--- js/activity-links.js
+++ js/activity-links.js
@@ -1,6 +1,13 @@
-// todo extract contact id
-// todo extract activity type
-// todo prepare a href by copying
 CRM.$(document).on('crmLoad', '.contact-activity-selector-activity', function() {
-  CRM.$('[data-entity=activity]').find('a[href*="atype=12"]').parent().append('<span>Set request</span>');
+  CRM.$.each(CRM.$('[data-entity=activity]').find('a[href*="atype=12"]').parent(), function(index, value) {
+    var aClass = CRM.$(value).find('a[title="View Activity"]').attr('class');
+    var link = CRM.$(value).find('a[title="View Activity"]').attr('href');
+    var id = gidipirusGetParam(link, 'id');
+    var cid = gidipirusGetParam(link, 'cid');
+    var newLink = '/civicrm/gidipirus/forgetme?reset=1&cid=' + cid + '&aid=' + id;
+    CRM.$(value).append('<a href="' + newLink + '" class="' + aClass + '" title="Register forget request">Register request</a>');
+  });
 });
+function gidipirusGetParam(link, name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(link)||[,""])[1].replace(/\+/g, '%20'))||null;
+}
