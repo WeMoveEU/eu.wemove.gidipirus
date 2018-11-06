665218bf1b7c1dabb9b688f906f9ecabc4279483
scardinius <scardinius@chords.pl>
2018-11-06 16:54:51 +0100

    Add dry_run mode to forget and cleanup api actions

--- api/v3/Gidipirus/Cleanup.php
+++ api/v3/Gidipirus/Cleanup.php
@@ -1,6 +1,15 @@
 <?php
+use CRM_Gidipirus_ExtensionUtil as E;
 
 function _civicrm_api3_gidipirus_cleanup_spec(&$spec) {
+  $spec['dry_run'] = [
+    'name' => 'dry_run',
+    'title' => E::ts('Dry run'),
+    'description' => E::ts('Only checking whether contact has valid Forgetme Fulfillment activity'),
+    'type' => CRM_Utils_Type::T_BOOLEAN,
+    'api.required' => 0,
+    'api.default' => FALSE,
+  ];
 }
 
 /**
@@ -13,6 +22,7 @@ function _civicrm_api3_gidipirus_cleanup_spec(&$spec) {
  */
 function civicrm_api3_gidipirus_cleanup(&$params) {
   $start = microtime(TRUE);
+  $dryRun = (bool) $params['dry_run'];
   $query = "SELECT DISTINCT ac.contact_id
             FROM civicrm_activity af
               JOIN civicrm_activity_contact ac ON ac.activity_id = af.id AND ac.record_type_id = 3
@@ -30,6 +40,7 @@ function civicrm_api3_gidipirus_cleanup(&$params) {
     $forgetParams = [
       'sequential' => 1,
       'contact_ids' => $contactIds,
+      'dry_run' => $dryRun,
     ];
     $result = civicrm_api3('Gidipirus', 'forget', $forgetParams);
     $values = $result['values'];
--- api/v3/Gidipirus/Forget.php
+++ api/v3/Gidipirus/Forget.php
@@ -9,6 +9,14 @@ function _civicrm_api3_gidipirus_forget_spec(&$spec) {
     'type' => CRM_Utils_Type::T_INT,
     'api.required' => 1,
   ];
+  $spec['dry_run'] = [
+    'name' => 'dry_run',
+    'title' => E::ts('Dry run'),
+    'description' => E::ts('Only checking whether contact has valid Forgetme Fulfillment activity'),
+    'type' => CRM_Utils_Type::T_BOOLEAN,
+    'api.required' => 0,
+    'api.default' => FALSE,
+  ];
 }
 
 /**
@@ -21,6 +29,7 @@ function _civicrm_api3_gidipirus_forget_spec(&$spec) {
  */
 function civicrm_api3_gidipirus_forget(&$params) {
   $start = microtime(TRUE);
+  $dryRun = (bool) $params['dry_run'];
   $contactIds = $params['contact_ids'];
   if (!is_array($contactIds)) {
     $contactIds = [$contactIds];
@@ -29,8 +38,13 @@ function civicrm_api3_gidipirus_forget(&$params) {
   foreach ($contactIds as $contactId) {
     try {
       $requestId = CRM_Gidipirus_Logic_Register::hasRequest($contactId);
-      $result = CRM_Gidipirus_Logic_Forget::anonymise($contactId);
-      CRM_Gidipirus_Logic_Register::complete($requestId);
+      if ($dryRun) {
+        $result = 1;
+      }
+      else {
+        $result = CRM_Gidipirus_Logic_Forget::anonymise($contactId);
+        CRM_Gidipirus_Logic_Register::complete($requestId);
+      }
       $values[$contactId] = [
         'id' => $contactId,
         'result' => $result,
