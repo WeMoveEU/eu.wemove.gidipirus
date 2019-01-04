82a6126b801d62c7e5c9b4a46b91742c6fddd817
scardinius <scardinius@chords.pl>
2019-01-04 13:00:59 +0100

    Add tests for other forgetme statuses

--- tests/phpunit/api/v3/StatusTest.php
+++ tests/phpunit/api/v3/StatusTest.php
@@ -36,4 +36,117 @@ class api_v3_StatusTest extends CRM_Gidipirus_BaseTest {
     $this->assertEquals(CRM_Gidipirus_Model_ForgetmeStatus::BLOCKED_VALUE, $result['values'][0]['status']);
   }
 
+  /**
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function testInProgress() {
+    self::emptyContact();
+    $requestedDate = new DateTime();
+    $params = [
+      'sequential' => 1,
+      'contact_ids' => self::$emptyContactId,
+      'channel' => CRM_Gidipirus_Model_RequestChannel::PERSONAL,
+      'requested_date' => $requestedDate->format('Y-m-d'),
+    ];
+    $result1 = $this->callAPISuccess('Gidipirus', 'register', $params);
+    $params = [
+      'sequential' => 1,
+      'contact_id' => self::$emptyContactId,
+    ];
+    $result = $this->callAPISuccess('Gidipirus', 'status', $params);
+    $this->assertEquals(CRM_Gidipirus_Model_ForgetmeStatus::IN_PROGRESS_VALUE, $result['values'][0]['status']);
+  }
+
+  /**
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function testObsolete() {
+    self::emptyContact();
+    $requestedDate = new DateTime();
+    $params = [
+      'sequential' => 1,
+      'contact_ids' => self::$emptyContactId,
+      'channel' => CRM_Gidipirus_Model_RequestChannel::PERSONAL,
+      'requested_date' => $requestedDate->format('Y-m-d'),
+    ];
+    $result1 = $this->callAPISuccess('Gidipirus', 'force', $params);
+    sleep(1);
+    $params = [
+      'sequential' => 1,
+      'contact_id' => self::$emptyContactId,
+    ];
+    $result = $this->callAPISuccess('Gidipirus', 'status', $params);
+    $this->assertEquals(CRM_Gidipirus_Model_ForgetmeStatus::OBSOLETE_VALUE, $result['values'][0]['status']);
+  }
+
+  /**
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function testCompleted() {
+    self::emptyContact();
+    $requestedDate = new DateTime();
+    $params = [
+      'sequential' => 1,
+      'contact_ids' => self::$emptyContactId,
+      'channel' => CRM_Gidipirus_Model_RequestChannel::PERSONAL,
+      'requested_date' => $requestedDate->format('Y-m-d'),
+    ];
+    $result1 = $this->callAPISuccess('Gidipirus', 'force', $params);
+    $params = [
+      'sequential' => 1,
+      'contact_ids' => self::$emptyContactId,
+    ];
+    $result2 = $this->callAPISuccess('Gidipirus', 'forget', $params);
+    $params = [
+      'sequential' => 1,
+      'contact_id' => self::$emptyContactId,
+    ];
+    $result = $this->callAPISuccess('Gidipirus', 'status', $params);
+    $this->assertEquals(CRM_Gidipirus_Model_ForgetmeStatus::COMPLETED_VALUE, $result['values'][0]['status']);
+  }
+
+  /**
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function testTooManyRequests() {
+    self::emptyContact();
+    $requestedDate = new DateTime();
+    $params = [
+      'sequential' => 1,
+      'contact_ids' => self::$emptyContactId,
+      'channel' => CRM_Gidipirus_Model_RequestChannel::PERSONAL,
+      'requested_date' => $requestedDate->format('Y-m-d'),
+    ];
+    $result1 = $this->callAPISuccess('Gidipirus', 'register', $params);
+    $result2 = $this->callAPISuccess('Gidipirus', 'register', $params);
+
+    $params = [
+      'sequential' => 1,
+      'contact_id' => self::$emptyContactId,
+    ];
+    $result = $this->callAPISuccess('Gidipirus', 'status', $params);
+    $this->assertEquals(CRM_Gidipirus_Model_ForgetmeStatus::TOO_MANY_REQUESTS_VALUE, $result['values'][0]['status']);
+  }
+
+  /**
+   * @throws \CiviCRM_API3_Exception
+   */
+  public function testNotApplicable() {
+    foreach (['Organization', 'Household'] as $contactType) {
+      $result = civicrm_api3('Contact', 'get', [
+        'sequential' => 1,
+        'contact_type' => $contactType,
+        'options' => ['limit' => 1],
+      ]);
+      if ($result['count']) {
+        $params = [
+          'sequential' => 1,
+          'contact_id' => $result['id'],
+        ];
+        $result = $this->callAPISuccess('Gidipirus', 'status', $params);
+        $this->assertEquals(CRM_Gidipirus_Model_ForgetmeStatus::NOT_APPLICABLE_VALUE, $result['values'][0]['status'], $contactType);
+      }
+    }
+  }
+
 }
