d5fc467ac03f8e6e6c05cbd381c675b13f50c957
scardinius <scardinius@chords.pl>
2019-02-07 16:24:51 +0100

    Force one link Register request

--- js/activity-links.js
+++ js/activity-links.js
@@ -1,11 +1,14 @@
 CRM.$(document).on('crmLoad', '.contact-activity-selector-activity', function() {
   CRM.$.each(CRM.$('[data-entity=activity]').find('a[href*="atype=12"]').parent(), function(index, value) {
-    var aClass = CRM.$(value).find('a[title="View Activity"]').attr('class');
-    var link = CRM.$(value).find('a[title="View Activity"]').attr('href');
-    var id = gidipirusGetParam(link, 'id');
-    var cid = gidipirusGetParam(link, 'cid');
-    var newLink = '/civicrm/gidipirus/forgetme?reset=1&cid=' + cid + '&aid=' + id;
-    CRM.$(value).append('<a href="' + newLink + '" class="' + aClass + '" title="Register forget request">Register request</a>');
+    var hasLink = CRM.$(value).find('a[href*="gidipirus"]');
+    if (!hasLink.length) {
+      var aClass = CRM.$(value).find('a[title="View Activity"]').attr('class');
+      var link = CRM.$(value).find('a[title="View Activity"]').attr('href');
+      var id = gidipirusGetParam(link, 'id');
+      var cid = gidipirusGetParam(link, 'cid');
+      var newLink = '/civicrm/gidipirus/forgetme?reset=1&cid=' + cid + '&aid=' + id;
+      CRM.$(value).append('<a href="' + newLink + '" class="' + aClass + '" title="Register forget request">Register request</a>');
+    }
   });
 });
 function gidipirusGetParam(link, name) {
